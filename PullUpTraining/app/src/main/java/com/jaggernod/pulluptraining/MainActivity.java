b2f4b807c7e51884a9974d8d3356a93d5270e632
Pawel Polanski <pawel.polanski@futurice.com>
2014-10-14 18:22:56 +0200

    Test code

--- PullUpTraining/app/src/main/java/com/jaggernod/pulluptraining/MainActivity.java
+++ PullUpTraining/app/src/main/java/com/jaggernod/pulluptraining/MainActivity.java
@@ -2,16 +2,51 @@ package com.jaggernod.pulluptraining;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+import rx.subscriptions.CompositeSubscription;
 
 
 public class MainActivity extends Activity {
+
+    private TextView textView;
+
+    private CompositeSubscription subscriptions = new CompositeSubscription();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.hello_world_text_view);
+        subscriptions.add(Observable.create((Subscriber<? super Integer> subscriber) -> {
+            if (!subscriber.isUnsubscribed()) {
+                int i = 0;
+                while (i < 100000) {
+                    subscriber.onNext(i++);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        subscriber.onError(e);
+                        break;
+                    }
+                }
+                subscriber.onCompleted();
+            }
+        })
+                .map(Object::toString)
+                .subscribeOn(Schedulers.computation())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe((t1) -> {
+                    Log.d("SSSS", this.hashCode() + "");
+                    textView.setText(t1);
+                }));
     }
 
 
@@ -35,5 +70,11 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        subscriptions.clear();
     }
 }
