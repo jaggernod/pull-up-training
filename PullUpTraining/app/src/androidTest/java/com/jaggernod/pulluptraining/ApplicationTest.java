e0fbe4ace7e4e91475e226a85e908214e70940d4
Pawel Polanski <pawel.polanski@futurice.com>
2014-10-14 17:18:30 +0200

    Initial tests added

--- PullUpTraining/app/src/androidTest/java/com/jaggernod/pulluptraining/ApplicationTest.java
+++ PullUpTraining/app/src/androidTest/java/com/jaggernod/pulluptraining/ApplicationTest.java
@@ -1,14 +1,17 @@
 package com.jaggernod.pulluptraining;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.annotation.Config;
 
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
-public class ApplicationTest extends ApplicationTestCase<Application> {
+import static org.junit.Assert.assertNotNull;
 
-    public ApplicationTest() {
-        super(Application.class);
+@Config(emulateSdk = 18)
+@RunWith(RobolectricGradleTestRunner.class)
+public class ApplicationTest {
+
+    @Test
+    public void testSomething() throws Exception {
+        assertNotNull(true);
     }
 }
---
+++ PullUpTraining/app/src/androidTest/java/com/jaggernod/pulluptraining/RobolectricGradleTestRunner.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2014 Samsung Electronics Co. Ltd All Rights reserved
+ */
+package com.jaggernod.pulluptraining;
+
+import org.junit.runners.model.InitializationError;
+import org.robolectric.AndroidManifest;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.res.Fs;
+
+public class RobolectricGradleTestRunner extends RobolectricTestRunner {
+
+    public RobolectricGradleTestRunner(Class<?> testClass) throws InitializationError {
+        super(testClass);
+    }
+
+    @Override protected AndroidManifest getAppManifest(Config config) {
+        String manifestProperty = System.getProperty("android.manifest");
+        if (config.manifest().equals(Config.DEFAULT) && manifestProperty != null) {
+            String resProperty = System.getProperty("android.resources");
+            String assetsProperty = System.getProperty("android.assets");
+            return new AndroidManifest(
+                    Fs.fileFromPath(manifestProperty), Fs.fileFromPath(resProperty),
+                    Fs.fileFromPath(assetsProperty));
+        }
+        return super.getAppManifest(config);
+    }
+}
+
