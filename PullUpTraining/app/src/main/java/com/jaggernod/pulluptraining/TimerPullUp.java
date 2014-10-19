ecf661767a0370ab794874e13689bc0da682afc7
Pawel Polanski <pawel.polanski@futurice.com>
2014-10-19 10:30:46 +0200

    Thread safe timer

--- PullUpTraining/app/src/main/java/com/jaggernod/pulluptraining/TimerPullUp.java
+++ PullUpTraining/app/src/main/java/com/jaggernod/pulluptraining/TimerPullUp.java
@@ -5,6 +5,8 @@ import android.os.Parcelable;
 import android.os.SystemClock;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
 
 import rx.Observable;
 
@@ -13,31 +15,32 @@ import rx.Observable;
  */
 public class TimerPullUp implements Parcelable {
 
-    private long startTime = -1;
-    private boolean running = false;
+    private AtomicLong startTime = new AtomicLong(-1);
+    private AtomicBoolean running = new AtomicBoolean(false);
 
     public Observable<Long> start() {
-        if (startTime < 0) {
-            startTime = SystemClock.elapsedRealtime();
+        if (startTime.get() < 0) {
+            startTime.set(SystemClock.elapsedRealtime());
         }
-        final long increment = (SystemClock.elapsedRealtime() - startTime) / 100;
+        final long increment = (SystemClock.elapsedRealtime() - startTime.get()) / 100;
         return Observable.timer(0, 100, TimeUnit.MILLISECONDS)
                 .map(aLong -> aLong + increment)
                 .doOnCompleted(this::stop)
                 .doOnError(throwable -> stop())
-                .doOnSubscribe(() -> running = true);
+                .doOnSubscribe(() -> running.set(true));
     }
 
     public void stop() {
-        running = false;
+        running.set(false);
     }
 
     public boolean isRunning() {
-        return running;
+        return running.get();
     }
 
     public long getTime() {
-        return startTime < 0 ? -1 : (SystemClock.elapsedRealtime() - startTime) / 100;
+        long time = startTime.get();
+        return time < 0 ? -1 : (SystemClock.elapsedRealtime() - time) / 100;
     }
 
     @Override
@@ -47,16 +50,16 @@ public class TimerPullUp implements Parcelable {
 
     @Override
     public void writeToParcel(Parcel dest, int flags) {
-        dest.writeLong(this.startTime);
-        dest.writeByte(running ? (byte) 1 : (byte) 0);
+        dest.writeLong(this.startTime.get());
+        dest.writeByte(this.running.get() ? (byte) 1 : (byte) 0);
     }
 
     public TimerPullUp() {
     }
 
     private TimerPullUp(Parcel in) {
-        this.startTime = in.readLong();
-        this.running = in.readByte() != 0;
+        this.startTime.set(in.readLong());
+        this.running.set(in.readByte() != 0);
     }
 
     public static final Parcelable.Creator<TimerPullUp> CREATOR
