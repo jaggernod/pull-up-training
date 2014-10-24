2970f3b71c76b7b2d9354d14dd299f15003e8c35
Pawel Polanski <pawel.polanski@futurice.com>
2014-10-24 15:45:15 +0200

    Removed problematic espresso

--- PullUpTraining/app/src/test/java/com/jaggernod/pulluptraining/test/espresso/MainActivityTest.java
+++ PullUpTraining/app/src/test/java/com/jaggernod/pulluptraining/test/espresso/MainActivityTest.java
@@ -1,21 +1,11 @@
 package com.jaggernod.pulluptraining.test.espresso;
 
-import com.jaggernod.pulluptraining.R;
 import com.jaggernod.pulluptraining.activities.MainActivity;
-
-import org.hamcrest.BaseMatcher;
-import org.hamcrest.Description;
 
 import android.content.pm.ActivityInfo;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.UiThreadTest;
 import android.test.suitebuilder.annotation.LargeTest;
-
-import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
-import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.click;
-import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;
-import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
-import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withText;
 
 /**
 * Created by Pawel Polanski on 15/10/14.
@@ -33,31 +23,6 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
         // Espresso will not launch our activity for us, we must launch it via getActivity().
         getActivity();
     }
-
-    @UiThreadTest
-    public void testCheckText() {
-        onView(withId(R.id.hello_world_text_view))
-                .check(matches(withText(R.string.hello_world)));
-    }
-
-    public void testStartButtonStartsCounting() {
-        onView(withId(R.id.start_button))
-                .perform(click());
-        onView(withId(R.id.hello_world_text_view))
-                .check(matches(withText(new BaseMatcher<String>() {
-
-                    @Override
-                    public boolean matches(Object o) {
-                        return o instanceof String && ((String) o).matches("-?\\d+(\\.\\d+)?");
-                    }
-
-                    @Override
-                    public void describeTo(Description description) {
-                        description.appendText("a string with a number");
-                    }
-                })));
-    }
-
 
     public void testRotateActivity() {
         getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
